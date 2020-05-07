75da31793810a9d44c8cc93a6e10a620955be32e
Long <thienlong666@gmail.com>
2020-05-08 00:06:37 +0700

    Commit source to test auto deploy jenkins

---
+++ login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Git</title>
+</head>
+<body>
+    <h1>Test git Merge</h1>
+    <p>Nhanh 1</p>
+	<p>Nhanh 2</p>
+	<p>Add them the p o branch 1</p>
+	<p>Nhanh 2</p>
+	<p>Nhanh 2</p>
+	<p>Nhanh 2</p>
+	<p>Nhanh 2</p>
+	<p>Nhanh 2</p>
+	<p>Test nhanh build code Jenkins</p>
+</body>
+</html>
